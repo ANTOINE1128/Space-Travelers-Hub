84508976fca1785c44923c8798dcb12d39b1d514
abenezer-addisu <dasesatube@gmail.com>
2023-06-28 16:27:04 +0300

    Mission Fetch Api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navmenu from './components/Navmenu';
 import Rockets from './pages/Rockets';
+import Missions from './pages/Mission';
 import Profile from './pages/Profile';
 import ErrorPage from './pages/ErrorPage';
 
@@ -12,6 +13,7 @@ function App() {
         <Navmenu />
         <Routes>
           <Route path="/" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketsReducer from './features/Rockets/RocketSlice';
+import MissionSlice from './missionSlice';
 
 const store = configureStore({
   reducer: {
     rockets: rocketsReducer,
+    missions: MissionSlice,
   },
 });
 
