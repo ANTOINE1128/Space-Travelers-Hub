2479a1128963a8792c4f9a2c566686a4c9f67d83
ANTOINE1128 <makdessyantoine@gmail.com>
2023-06-29 22:46:17 +0300

    fixing rocket

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
 import Rocket from '../components/Rocket';
 import { fetchRockets } from '../redux/features/Rockets/RocketSlice';
+import '../App.css';
 
 const Rockets = () => {
-  const rockets = useSelector(selectRockets);
+  const rockets = useSelector((store) => store.rockets);
   const dispatch = useDispatch();
   const lengthRock = rockets.rocket.length;
   useEffect(() => {
