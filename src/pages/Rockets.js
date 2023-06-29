0b4e2ffcf4833a6c8be5298903370c9983aeb76c
ANTOINE1128 <makdessyantoine@gmail.com>
2023-06-29 21:34:01 +0300

    add all branches

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,14 +1,16 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchRockets } from '../redux/features/Rockets/RocketSlice';
 import Rocket from '../components/Rocket';
+import { fetchRockets } from '../redux/features/Rockets/RocketSlice';
 
 const Rockets = () => {
   const rockets = useSelector((store) => store.rockets);
   const dispatch = useDispatch();
+  const lengthRock = rockets.rocket.length;
   useEffect(() => {
+    if (lengthRock > 0) return;
     dispatch(fetchRockets());
-  }, [dispatch]);
+  }, [dispatch, lengthRock]);
   return (
     <div className="container">
       {rockets.loading && <div>loading</div>}
@@ -26,6 +28,7 @@ const Rockets = () => {
             img={rocket.flickr_images}
             id={rocket.id}
             desc={rocket.description}
+            reserved={rocket.reserved}
           />
         ))
       ) : null}
